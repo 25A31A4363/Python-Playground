1b8b2bbda00b20e944e8e104a49dc9357269df3e
Mohith Sripathi <sripathi.mohith0408@gmail.com>
2026-07-20 22:40:54 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,16 @@
+name = "Mohith"
+college = "Pragati Engineering College"
+branch = "CSE-AI"
+section = 'A'
+Rollno = "25A31A4363"
+age = 17
+
+#To display details of Student
+
+print("Name of the Student: ",name)
+print("College: ",college)
+print("Branch: ",branch)
+print("Section: ",section)
+print("Roll Number: ",Rollno)
+print("Age: ",age)
+
